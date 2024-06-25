8a156472350609b49b526c40aeaf87365934676d
antikab <arsprosvet@mail.ru>
2024-06-26 01:53:23 +0300

    изменил стиль кнопки удаления превью картинки

--- src/app/[viewStyleId]/edit/page.js
+++ src/app/[viewStyleId]/edit/page.js
@@ -75,15 +75,19 @@ function ImageUploader({ onDrop, preview, onRemove }) {
   return (
     <div
       {...getRootProps({
-        className: `size-96 flex justify-center items-center border-dashed border-2 hover:cursor-pointer ${
+        className: `size-96 flex justify-center items-center border-dashed border-2 hover:cursor-pointer hover:border-gray-400 ${
           isDragActive
-            ? 'border-blue-500 bg-blue-500/10 focus:border-blue-500 hover:border-blue-500 '
+            ? 'border-blue-500 bg-blue-500/10 focus:border-blue-500 hover:border-blue-500 text-blue-500'
             : 'border-gray-300'
-        } hover:border-gray-400`,
+        }`,
       })}
     >
       <input {...getInputProps()} />
-      {preview ? (
+      {isDragActive ? (
+        <span className="text-blue-500 text-center font-semibold ">
+          Отпустите изображение
+        </span>
+      ) : preview ? (
         <div className="relative">
           <Image
             className="object-contain h-[380px]"
@@ -96,13 +100,12 @@ function ImageUploader({ onDrop, preview, onRemove }) {
             <svg
               onClick={handleRemoveImage}
               style={{ cursor: 'pointer' }}
-              className="flex absolute mx-1 top-0 right-0 self-start size-5 text-gray-500 hover:text-red-500"
+              className="flex absolute m-1 top-0 right-0 self-start size-6 text-gray-500 hover:text-red-500 hover:bg-red-100 bg-white border border-gray-200 hover:border-red-200 rounded-lg shadow-sm"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 20 20"
+              viewBox="0 0 24 24"
               strokeWidth="2"
               stroke="currentColor"
-              cursor="grab"
             >
               <path
                 strokeLinecap="round"
@@ -114,8 +117,7 @@ function ImageUploader({ onDrop, preview, onRemove }) {
         </div>
       ) : (
         <span className="text-gray-500 text-center">
-          Перетащите изображение <br />
-          для загрузки
+          Перетащите изображение <br />в область загрузки
         </span>
       )}
     </div>
