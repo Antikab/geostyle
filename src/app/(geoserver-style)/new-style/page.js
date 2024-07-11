0c977f92c5512aefa5d69a4214961dd9967de326
antikab <arsprosvet@mail.ru>
2024-07-11 04:45:25 +0300

    edit

--- src/app/(geoserver-style)/new-style/page.js
+++ src/app/(geoserver-style)/new-style/page.js
@@ -9,7 +9,7 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
-// Схема валидации Yup
+// Схема валидации Yup.
 const validationSchema = yup.object().shape({
   name: yup
     .string()
